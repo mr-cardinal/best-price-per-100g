32038912d204e992aa71bcbc59b0b0448362c321
mr-cardinal <mr.cardinal.wolf@gmail.com>
2026-09-08 17:02:13 +0300

    Update product price page

--- script.js
+++ script.js
@@ -9,6 +9,28 @@ function started() {
     document.body.append(button);
 
     button.addEventListener('click', buttonClick);
+
+    const products = getProducts();
+    if (products.length > 0 && products !== null) {
+        products.forEach((product) => {
+            element = document.createElement('div');
+            element.id = product.id;
+            element.className = "product";
+            const productHtml = `
+                        <div class="product__rank"></div>
+                        <div class="product__photo"></div>
+                        <div class="product__name">${product.name}</div>
+                        <div class="product__unit">${product.quantity.unit}</div>
+                        <div class="product__price">${product.price}</div>
+                        <div class="product__unitPrice"></div>
+                        <div class="product__isBest"></div>
+                        <div class="product__delite"></div>
+                    
+                `;
+            element.innerHTML = productHtml;
+            document.getElementById("products_container").insertAdjacentElement('afterbegin', element);
+        });
+    }
 }
 
 // клик по кнопке
@@ -51,6 +73,68 @@ function createValue(idElement) {
 }
 
 const values = new Map();
+const products = [
+    {
+        id: "550e8400-e29b-41d4-a716-446655440000",
+        name: "Греча от мироторг",
+        photo: null,
+
+        quantity: {
+            value: 800,
+            unit: "грамм",
+            type: "Масса"
+        },
+
+        price: 120,
+
+        createdAt: Date.now()
+    },
+    {
+        id: "550e8400-e29b-41d4-a716-446655440001",
+        name: "Греча от мироторг 2",
+        photo: null,
+
+        quantity: {
+            value: 600,
+            unit: "грамм",
+            type: "Масса"
+        },
+
+        price: 620,
+
+        createdAt: Date.now()
+    },
+    {
+        id: "550e8400-e29b-41d4-a716-446655440003",
+        name: "Греча от мироторг 3",
+        photo: null,
+
+        quantity: {
+            value: 50,
+            unit: "грамм",
+            type: "Масса"
+        },
+
+        price: 20,
+
+        createdAt: Date.now()
+    },
+    {
+        id: "550e8400-e29b-41d4-a716-446655440004",
+        name: "Греча от мироторг 4",
+        photo: null,
+
+        quantity: {
+            value: 680,
+            unit: "грамм",
+            type: "Масса"
+        },
+
+        price: 520,
+
+        createdAt: Date.now()
+    }
+];
 
 // наполнение полей ввода
 function createInput(inputName, name, id) {
@@ -123,4 +207,17 @@ function bestResultColor(id) {
 }
 function removeResultColor(id) {
     document.querySelector(`[data-id="${id}"]`).style.color = 'black';
+}
+
+function getProducts() {
+    return JSON.parse(
+        localStorage.getItem('products') || '[]'
+    );
+}
+
+function saveProducts(products) {
+    localStorage.setItem(
+        'products',
+        JSON.stringify(products)
+    );
 }
